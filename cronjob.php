157400ce5e9c2efaf58b41bc872871c701d57306
hutt <jannis@hutt.io>
2020-01-14 21:05:38 +0100

    added cronjob script prototype for test purposes

--- cronjob.php
+++ cronjob.php
@@ -9,3 +9,25 @@
 	@description retrieve schedule from bundestag.de, parse it, save it to database
 
 */
+
+require("to.php");
+
+/*
+	Test-Beispiel:
+*/
+
+//Status der DB checken
+$todb = new TODB();
+
+if (!$todb->checkStatus()) {
+	$todb->createDB();
+}
+
+// Daten für Kalenderwoche 3
+$data = new FetchTOs(3, 2020)->fetch();
+$parse = new TOParser($data);
+
+if($parse){
+	//Parsen erfolgreich
+	echo "Neue Daten erfolgreich geparst.";
+}
