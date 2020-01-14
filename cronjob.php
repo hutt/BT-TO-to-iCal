7e10eb4b3c64507a2e3e9f1a21ed10f24d5dea66
hutt <jannis@hutt.io>
2020-01-14 16:57:01 +0100

    added skript file to fetch updates

---
+++ cronjob.php
@@ -0,0 +1,11 @@
+<?php
+
+/*	
+	BT-TO-to-iCal
+	Copyright (C) 2020 Jannis Hutt
+
+	@author Jannis Hutt
+	@email github@jh0.eu
+	@description retrieve schedule from bundestag.de, parse it, save it to database
+
+*/
